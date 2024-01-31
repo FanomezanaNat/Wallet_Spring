af69b9e5ba5e2a535a1ce8999742c9e2d4313b3c
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-01-31 09:04:26 +0300

    feat: add config files for project

---
+++ src/main/java/com/hei/wallet_spring/WalletSpringApplication.java
@@ -0,0 +1,13 @@
+package com.hei.wallet_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hei/wallet_spring/WalletSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.wallet_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
